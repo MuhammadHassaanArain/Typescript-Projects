cdfb7439739ea7a3b7562e3eff26e63e9507aaaa
MuhammadHassaanArain <rafatnaz21@gmail.com>
2024-03-16 21:06:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "enter first digit",
+        type: "number",
+        name: "firstnumber",
+    },
+    {
+        message: "enter second digit",
+        type: "number",
+        name: "secondnumber",
+    },
+    {
+        message: "select one of the operator to perform acrion",
+        type: "list",
+        name: "operators",
+        choices: ["addition", "substraction", "multiplication", "division"],
+    },
+]);
+if (answer.operators === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operators === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operators === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operators === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    ("Please select valid operator");
+}
